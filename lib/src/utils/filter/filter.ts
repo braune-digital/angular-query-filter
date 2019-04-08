80447edb2038cbe520a574cc77bc3a308a175fcd
Felix Baltruschat <fb@braune-digital.com>
2019-04-08 17:39:01 +0200

    add name to filter

--- lib/src/utils/filter/filter.ts
+++ lib/src/utils/filter/filter.ts
@@ -4,9 +4,14 @@ export abstract class Filter {
     public id: string;
     public type: string;
     public active = true;
+    public name = '';
     public refreshEvent = new Subject<boolean>();
 
     constructor() {
+    }
+
+    public toJson() {
+        return { ...this.get(), name: this.name };
     }
 
     public abstract get(): object;
